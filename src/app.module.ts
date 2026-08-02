2db35fcf3f14d0945b36d8058115ada5309ae70c
Ayyubxon <ayyubxonkamoldinovcoder@gmail.com>
2026-08-02 18:19:39 +0500

    add @/* path alias for src imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
-import { typeOrmConfig } from './config/typeorm.config';
+import { typeOrmConfig } from '@/config/typeorm.config';
 
 @Module({
   imports: [
