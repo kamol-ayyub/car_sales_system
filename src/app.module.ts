c58c48528f12c6e7e0a41fef4030ec2b99368656
Ayyubxon <ayyubxonkamoldinovcoder@gmail.com>
2026-08-02 17:05:19 +0500

    add config module and validation pipe setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
