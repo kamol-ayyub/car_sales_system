8575b280bcd857734451030ff3840e8cccd8cf15
Ayyubxon <ayyubxonkamoldinovcoder@gmail.com>
2026-09-08 18:32:56 +0500

    complete simple crud app with basic functionalities

--- src/user/auth/register.dto.ts
+++ src/user/auth/register.dto.ts
@@ -1,31 +1,9 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { PickType } from '@nestjs/mapped-types';
+import { CreateUserDto } from '../dto/create-user.dto';
 
-export class RegisterDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(100)
-  name: string;
-
-  @IsEmail()
-  @IsNotEmpty()
-  @MaxLength(255)
-  email: string;
-
-  @IsOptional()
-  @IsString()
-  @MaxLength(20)
-  phone?: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @MaxLength(72)
-  password: string;
-}
+export class RegisterDto extends PickType(CreateUserDto, [
+  'name',
+  'email',
+  'phone',
+  'password',
+] as const) {}
